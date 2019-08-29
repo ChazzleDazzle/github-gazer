ecebeb445e5abfd8cd28c78c18be25b2ae3da075
Charlie Gibson <gibson.charlie@gmail.com>
2019-08-29 13:11:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.2",
     description="Watch GitHub for my review requests, or pull requests to release branches, and notify over Slack.",
     author="Charlie Gibson",
-    author_email="gibson@csdisco.com",
+    author_email="gibson.charlie@gmail.com",
     packages=["gazer"],
     license="MIT",
     install_requires=["PyGithub", "PyYAML", "requests"],
